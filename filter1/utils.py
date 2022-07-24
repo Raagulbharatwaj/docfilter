0631c600ff94bc077a4873cab62ffe7e6214ae43
Raagulbharatwaj <74106637+Raagulbharatwaj@users.noreply.github.com>
2022-07-24 18:05:48 +0530

    Final Commit

--- filter1/utils.py
+++ filter1/utils.py
@@ -5,14 +5,14 @@ import os
 def handle_uploaded_file(file,keywords):
     fs = FileSystemStorage()
     name = fs.save(file.name,file)
-    with open(fs.path(name),'r') as f:
-        reader = csv.reader(f)
-        next(reader)
-        content = []
-        for row in reader:
-            content.append(" ".join([strs for strs in row]))
-        print(content)
-    with open(fs.path(name),'r') as f1 ,open(fs.path(name)+"removed",'a') as f2:
+    file_path = fs.path(name)
+    path = file_path.split('\\')
+    path.pop(-1)
+    folder_path = ""
+    for i in path:
+        folder_path+=f"{i}\\"
+    new_name = name.split('.')[0]+"_removed.csv"
+    with open(fs.path(name),'r') as f1 ,open(fs.path(new_name),'a') as f2:
         reader = csv.reader(f1)
         writer = csv.writer(f2)
         count = 0
@@ -20,9 +20,20 @@ def handle_uploaded_file(file,keywords):
             if count==0:
                 row.append("Result")
                 writer.writerow(row)
+                count+=1
             else:
-                writer.writerow(row.append("hello"))
-    print("success")
-    return name
+                flag = False
+                for item in row:
+                    for keyword in keywords:
+                        if keyword in item:
+                            flag = True
+                            break
+                if flag:
+                    row.append("KEYWORD PRESENT")
+                    writer.writerow(row)
+                else:
+                    row.append("NO KEYWORD FOUND")
+                    writer.writerow(row)
+    return new_name
 
 
